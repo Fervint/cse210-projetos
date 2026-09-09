a15464cdf7241589fe4879fa13a16f4b3c6b926c
Fervint <fricardodasilvamuniz@byupathway.edu>
2026-09-09 06:44:46 -0300

    substituir mensagem de saudação por solicitação de nome e sobrenome

--- semana01/Exercicio1/Program.cs
+++ semana01/Exercicio1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio1.");
+        Console.Write("Qual é seu primeiro nome? ");
+        string primeiroNome = Console.ReadLine();
+
+        Console.Write("Qual é seu sobrenome? ");
+        string sobrenome = Console.ReadLine();
+
+        Console.WriteLine($"Seu nome é {sobrenome}, {primeiroNome} {sobrenome}!");
     }
 }
